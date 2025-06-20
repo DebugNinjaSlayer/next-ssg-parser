fefe73e2be6299db8ce7cac607c9cf28f87686c5
DebugNinjaSlayer <8620373+DebugNinjaSlayer@users.noreply.github.com>
2025-06-20 17:41:57 +0800

    get next ssg content json object

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello, world!");
+import example from "./example.json" with { type: "json" };
+
+const { props: { pageProps: { content } } } = example;
+
+const nextContent = JSON.parse(content);
+
+console.log(nextContent);
